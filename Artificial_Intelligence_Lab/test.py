110a01fb68709d2df5f55154226b2427dbbeb3a9
mdrehantabish123 <rehan.shekh24@mmit.edu.in>
2025-07-14 13:52:38 +0530

    test.py

--- Artificial_Intelligence_Lab/test.py
+++ Artificial_Intelligence_Lab/test.py
@@ -1,1 +1,11 @@
-print("Hello")
+#program for whether the given number is positive,negative or zero.
+number=float(input("Enter a number:-"))
+
+if number>0:
+  print("The Number is Positive.")
+
+elif number<0:
+  print("The Number is Negative.")
+
+else:
+  print("The Number is Zero.")
